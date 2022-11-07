15601e6682b17625469cb2500e541a60c09fdbbc
avipriyaghosh487 <aceghosh487@gmail.com>
2022-11-07 20:24:09 +0530

    Added http intro module.
    A basic intro to http module.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+const http = require('http');
 
-const names = require('./4-names');
-const sayHi =require('./5-utils');
-const data = require('./6-alternative-flavor');
-console.log(data);
+const server=http.createServer((req,res) => {
+    if(req.url === '/')
+    {
+        res.end('Welcome to the home page.')
+    }
+    if(req.url ==='/about')
+    {
+        res.end('Here is our short history.')
+    }
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">Back home></a>
+    `)
+})
 
-//require('./7-mind-grenade');
-//sayHi(secret);
-//sayHi(names.john);
-//sayHi(names.peter);
-//sayHi('Avi');
+server.listen(5000)
